5e7540acad232f2af309164d90d9e3e5063aa77f
Poly-Etilen <wlsdud04060@naver.com>
2026-07-15 13:44:09 +0900

    first commit

---
+++ src/main/java/site/yesaido/ai_server/AiServerApplication.java
@@ -0,0 +1,13 @@
+package site.yesaido.ai_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiServerApplication.class, args);
+    }
+
+}
