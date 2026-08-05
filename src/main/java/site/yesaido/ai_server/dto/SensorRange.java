bf057cda3e2ba3f339b7cba9649107c8dbf71c95
kjs31602 <kjs31602@gmail.com>
2026-08-05 12:56:35 +0900

    min, max 방어 코드 추가

--- src/main/java/site/yesaido/ai_server/dto/SensorRange.java
+++ src/main/java/site/yesaido/ai_server/dto/SensorRange.java
@@ -4,4 +4,9 @@ public record SensorRange(
         Double min,
         Double max
 ) {
+    public SensorRange{ // .isFinite() 무한대 방지
+        if(min == null || max == null || !Double.isFinite(min) || !Double.isFinite(max) || min > max){
+            throw new IllegalArgumentException("잘못된 범위 저장");
+        }
+    }
 }
