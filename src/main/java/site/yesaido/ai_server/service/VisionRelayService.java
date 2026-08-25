58efd7e531330691faff80af1bd8cd29d0973ee1
{kim75503} <kim755030@naver.com>
2026-08-25 13:27:35 +0900

    동기화

--- src/main/java/site/yesaido/ai_server/service/VisionRelayService.java
+++ src/main/java/site/yesaido/ai_server/service/VisionRelayService.java
@@ -3,7 +3,6 @@ package site.yesaido.ai_server.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import site.yesaido.ai_server.storage.minio.service.MinioImageService;
 import site.yesaido.ai_server.client.VisionClient;
 import site.yesaido.ai_server.dto.vision.response.VisionResponse;
 
@@ -12,7 +11,6 @@ import site.yesaido.ai_server.dto.vision.response.VisionResponse;
 public class VisionRelayService {
 
     private final VisionClient visionClient;
-    private final MinioImageService minioImageService;
 
     public VisionResponse analyzeMushroomHealth(MultipartFile image) {
         return visionClient.analyzeMushroomHealth(image);
